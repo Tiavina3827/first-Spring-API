2733694d94288745f9ab0b9d2ec8aef2f9b9812f
Tiavina3827 <hei.tiavina.4@gmail.com>
2024-09-07 23:08:19 +0300

    feat: Search Todo by id endpoint

--- src/main/java/org/zavatra/todoj1/API/endpoints.java
+++ src/main/java/org/zavatra/todoj1/API/endpoints.java
@@ -1,7 +1,6 @@
 package org.zavatra.todoj1.API;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.zavatra.todoj1.classes.ToDo;
 import org.zavatra.todoj1.methods.SQLMethods;
 
@@ -16,5 +15,9 @@ public class endpoints {
     @GetMapping("/todos")
     public List<ToDo> getToDos(){
         return (SQLMethods.selectAll());
+    }
+    @GetMapping("/todos/{id}")
+    public ToDo getToDo(@PathVariable int id){
+        return (SQLMethods.selectById(id));
     }
 }
